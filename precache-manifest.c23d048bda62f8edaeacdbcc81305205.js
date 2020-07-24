3b69d61e85ddda7966959d62849304e43e85109a
vonhoro <64147799+vonhoro@users.noreply.github.com>
2020-07-24 19:50:34 -0400

    Updates

---
+++ precache-manifest.c23d048bda62f8edaeacdbcc81305205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984339cc20a78f3165f25dcf83dfa19e",
+    "url": "/Battleships/index.html"
+  },
+  {
+    "revision": "4558f72f64d3d7409aa3",
+    "url": "/Battleships/static/css/main.03d7aa45.chunk.css"
+  },
+  {
+    "revision": "3cffc0e0730ebb354144",
+    "url": "/Battleships/static/js/2.653c5b06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Battleships/static/js/2.653c5b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4558f72f64d3d7409aa3",
+    "url": "/Battleships/static/js/main.658696df.chunk.js"
+  },
+  {
+    "revision": "1f0023cda2cd23ae3fa0",
+    "url": "/Battleships/static/js/runtime-main.b1d3826a.js"
+  }
+]);
